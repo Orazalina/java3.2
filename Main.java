f9bfa8636899d5a220f21e40c2c9590df0201b63
Anara Orazalina <orazalina.anara@inbox.ru>
2021-01-24 21:12:36 +0600

    main corrected

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int initial_balance = 100;
+        int InitialBalance = 100;
         int receipt = 100;
         int bonus = receipt / 100;
+
         if (receipt > 1000){
             bonus = receipt / 100;
         }
         else {
             bonus = 0;
         }
-        int balance = initial_balance + receipt + bonus;
+
+        int balance = InitialBalance + receipt + bonus;
+
         System.out.println(balance);
     }
 }
